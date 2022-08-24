f306639fe15473cd483c854aa01fe380a915e3af
M0hamedSharaf <sazxsazx987987@gmail.com>
2022-08-24 03:31:57 +0200

    edite time input grop

--- app/Http/Requests/Group/UpdateGroupRequest.php
+++ app/Http/Requests/Group/UpdateGroupRequest.php
@@ -24,8 +24,8 @@ class UpdateGroupRequest extends FormRequest
     public function rules()
     {
         return [
-            'from' => 'required',
-            'to' => 'required',
+            'from' => 'required|date_format:H:i',
+            'to' => 'required|date_format:H:i|after:from',
             'teacher_id' => 'required',
             'age_type' => 'required',
            
