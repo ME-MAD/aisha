b112f4eb6e228c21715cdbbc85c081f7aa725da8
M0hamedSharaf <sazxsazx987987@gmail.com>
2022-09-01 11:34:55 +0200

    in group type crud change input price to be like num_of_days

--- database/migrations/2022_08_14_200330_create_group_types_table.php
+++ database/migrations/2022_08_14_200330_create_group_types_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('group_types', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->double('price');
+            $table->text('price');
             $table->unsignedInteger('days_num');
             $table->timestamps();
         });
