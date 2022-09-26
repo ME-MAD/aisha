a8535951d8b4c10f86cce40b330174d5ec3e2b45
M0hamedSharaf <sazxsazx987987@gmail.com>
2022-09-26 11:51:30 +0200

    Qraan list created

--- resources/views/pages/student/partials/subject.blade.php
+++ resources/views/pages/student/partials/subject.blade.php
@@ -12,68 +12,59 @@
     <div class="table-responsive">
         <div class="row" style="height: 400px">
             <div class="col-4">
-              <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
-                <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
-                <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
-              </div>
+                <div class="list-group" id="list-tab" role="tablist">
+                    @foreach ($subjects as $subject)
+                        <a class="list-group-item list-group-item-action" id="listhome" data-toggle="listhome"
+                            href="#list-home{{ $subject->id }}" role="tab"
+                            aria-controls="home">{{ $subject->name }}</a>
+                    @endforeach
+
+
+                </div>
             </div>
             <div class="col-8">
-              <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
-                    <ul class="list-group task-list-group">
-                        <li class="list-group-item list-group-item-action">
-                            <div class="n-chk">
-                                <label class="new-control new-checkbox checkbox-primary w-100 justify-content-between">
-                                  <input type="checkbox" class="new-control-input">
-                                  <span class="new-control-indicator"></span>
-                                    <span class="ml-2">
-                                        List groups are a flexible and powerful component for displaying simple.
-                                    </span>
-                                    <span class="ml-3 d-block">
-                                        <span class="badge badge-secondary">Project</span>
-                                    </span>
-                                </label>
-                            </div>
-                        </li>
-                        <li class="list-group-item list-group-item-action active">
-                            <div class="n-chk">
-                                <label class="new-control new-checkbox checkbox-primary w-100 justify-content-between">
-                                  <input type="checkbox" class="new-control-input">
-                                  <span class="new-control-indicator"></span>
-                                    <span class="ml-2">
-                                        List groups are a flexible and powerful component for displaying simple.
-                                    </span>
-                                    <span class="ml-3 d-block">
-                                        <span class="badge badge-primary">Urgent</span>
-                                    </span>
-                                </label>
-                            </div>
-                        </li>
-                        <li class="list-group-item list-group-item-action">
-                            <div class="n-chk">
-                                <label class="new-control new-checkbox checkbox-primary w-100 justify-content-between">
-                                  <input type="checkbox" class="new-control-input">
-                                  <span class="new-control-indicator"></span>
-                                      <span class="ml-2">List groups are a flexible and powerful component for displaying simple.</span>
-                                      <span class="ml-3 d-block">
-                                          <span class="badge badge-success">Success</span>
-                                      </span>
-                                </label>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" role="tabpanel" aria-labelledby="list-home-list">
+                        <ul class="list-group task-list-group" id="list" style="display: none;">
+                            @foreach ($lessons as $lesson)
+                                <li class="list-group-item list-group-item-action">
+                                    <div class="n-chk" id="list-home{{ $lesson->subject_id }}">
+                                        <label
+                                            class="new-control new-checkbox checkbox-primary w-100 justify-content-between">
+                                            <input type="checkbox" class="new-control-input">
+                                            <span class="new-control-indicator"></span>
+                                            <span class="ml-2">
+                                                {{ $lesson->title }}
+                                            </span>
+                                            <span class="ml-3 d-block">
+                                                <span class="badge badge-secondary">Project</span>
+                                            </span>
+                                        </label>
+                                    </div>
+                                </li>
+                            @endforeach
+
+                        </ul>
+                    </div>
+                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                        <h1>hello this is Two</h1>
+                    </div>
+
                 </div>
-                <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-                    <h1>hello this is Two</h1>
-                </div>
-                <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-                <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-              </div>
             </div>
-          </div>
+        </div>
     </div>
 </div>
 
 
+@section('javascript')
+    <script>
+        $(document).ready(function() {
+
+            $('#listhome').click(function() {
+                $('#list').show();
+            });
+
+        })
+    </script>
+@endsection
