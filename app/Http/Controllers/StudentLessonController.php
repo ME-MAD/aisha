7b362dc7ac3500f0cecc10d40190533d40a595e4
M0hamedSharaf <sazxsazx987987@gmail.com>
2022-12-13 21:59:22 +0200

    display a timeline of studentLessonReview and SyllabusReview in student_lesson_show

--- app/Http/Controllers/StudentLessonController.php
+++ app/Http/Controllers/StudentLessonController.php
@@ -65,8 +65,13 @@ class StudentLessonController extends Controller
      */
     public function show(StudentLesson $studentLesson)
     {
-        return view('pages.studentLesson.show',[
-            'studentLesson' => $studentLesson->load(['syllabus','lesson.subject','student'])
+        $studentLesson->load(['syllabus', 'lesson.subject', 'student',  'studentLessonReview.syllabusReviews']);
+
+        $studentLessonReviews = $studentLesson->studentLessonReview->get();
+
+        return view('pages.studentLesson.show', [
+            'studentLesson' => $studentLesson,
+            'studentLessonReviews' => $studentLessonReviews
         ]);
     }
 
--- resources/views/pages/studentLesson/show.blade.php
+++ resources/views/pages/studentLesson/show.blade.php
@@ -5,6 +5,11 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('adminAssets/plugins/table/datatable/datatables.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('adminAssets/plugins/table/datatable/custom_dt_html5.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('adminAssets/plugins/table/datatable/dt-global_style.css') }}">
+    <!--  BEGIN CUSTOM STYLE FILE  -->
+    <link href="{{ asset('adminAssets/assets/css/scrollspyNav.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('adminAssets/assets/css/components/timeline/custom-timeline.css') }}" rel="stylesheet"
+        type="text/css" />
+    <!--  END CUSTOM STYLE FILE  -->
 @endsection
 
 @section('breadcrumb')
@@ -42,189 +47,19 @@
             <div class="col-lg-12">
                 <div class="statbox widget box box-shadow">
                     <div class="bio layout-spacing ">
-                        <div class="widget-content widget-content-area">
-                            <div class="bio-skill-box">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Subject</h5>
-                                                <p>{{ $studentLesson->lesson->subject->name ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Lesson</h5>
-                                                <p>{{ $studentLesson->lesson->title ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Student</h5>
-                                                <p>{{ $studentLesson->student->name ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Group</h5>
-                                                <p>{{ $studentLesson->group->name ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('pages.studentLesson.partials.sectionOne')
                     </div>
                     <div class="bio layout-spacing ">
-                        <div class="widget-content widget-content-area">
-                            <h3 class="">Lesson : {{ $studentLesson->lesson->title ?? '' }}</h3>
-
-                            <div class="bio-skill-box">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Chapters Count</h5>
-                                                <p>{{ $studentLesson->lesson->chapters_count ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Lesson Starts From</h5>
-                                                <p>{{ $studentLesson->lesson->from_page ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Lesson Ends At</h5>
-                                                <p>{{ $studentLesson->lesson->to_page ?? '' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('pages.studentLesson.partials.sectionTwo')
                     </div>
                     <div class="bio layout-spacing ">
-                        <div class="widget-content widget-content-area">
-                            <div class="bio-skill-box">
-                                <div class="row">
-                                    <div class="col-3">
-                                        <div
-                                            class="d-flex b-skills {{ $studentLesson->finished ? 'bg-success' : 'bg-danger' }}">
-                                            <div class="">
-                                                <h5 style="color:white">Finished That Lesson</h5>
-                                                <p style="color:white">
-                                                    {{ $studentLesson->finished ? 'Finished' : 'Not Finished' }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-3">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Percentage</h5>
-                                                <p>{{ $studentLesson->percentage }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-3">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Last Chapter Finished</h5>
-                                                <p>{{ $studentLesson->last_chapter_finished }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-3">
-                                        <div class="d-flex b-skills">
-                                            <div class="">
-                                                <h5>Last Page Finished</h5>
-                                                <p>{{ $studentLesson->last_page_finished }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('pages.studentLesson.partials.sectionThird')
+                    </div>
+                    <div class="bio layout-spacing ">
+                        @include('pages.studentLesson.partials.studentLessonReview')
                     </div>
                     <div class="widget-content widget-content-area br-6">
-                        <h2 class="alert alert-primary" role="alert">
-                            New Lessons
-                        </h2>
-                        <div class="table-responsive mb-4 mt-4">
-                            <div id="html5-extension_wrapper"
-                                class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer">
-                                <div class="row">
-
-                                    <div class="col-md-12">
-                                        <table id="html5-extension"
-                                            class="table table-hover non-hover dataTable no-footer" style="width: 100%;"
-                                            role="grid" aria-describedby="html5-extension_info">
-                                            <thead>
-                                                <tr role="row">
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Position: activate to sort column ascending"
-                                                        style="width: 197px;">From Chapter</th>
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Office: activate to sort column ascending"
-                                                        style="width: 85px;">To Chapter</th>
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Age: activate to sort column ascending"
-                                                        style="width: 32px;">From Page</th>
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Start date: activate to sort column ascending"
-                                                        style="width: 95px;">To Page</th>
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Start date: activate to sort column ascending"
-                                                        style="width: 95px;">Rate</th>
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Salary: activate to sort column ascending"
-                                                        style="width: 61px;">Finished</th>
-                                                    <th class="sorting" tabindex="0" aria-controls="html5-extension"
-                                                        rowspan="1" colspan="1"
-                                                        aria-label="Salary: activate to sort column ascending"
-                                                        style="width: 61px;">Date</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach ($studentLesson->syllabus as $syllab)
-                                                    <tr role="row">
-                                                        <td>{{ $syllab->from_chapter }}</td>
-                                                        <td>{{ $syllab->to_chapter }}</td>
-                                                        <td>{{ $syllab->from_page }}</td>
-                                                        <td>{{ $syllab->to_page }}</td>
-                                                        <td>{{ $syllab->rate }}</td>
-                                                        @if ($syllab->finished == 1)
-                                                            <td><span class="badge badge-success"> Completed
-                                                                </span></td>
-                                                        @else
-                                                            <td><span class="badge badge-danger">Not Completed </span></td>
-                                                        @endif
-                                                        <td>{{ $syllab->created_at }}</td>
-                                                    </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('pages.studentLesson.partials.newLessons')
                     </div>
                 </div>
             </div>
