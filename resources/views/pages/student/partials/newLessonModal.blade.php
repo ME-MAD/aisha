c65d3bf38d7ac0886e99c02bae66816f6600ffbb
AhmedKamal148 <a.kamal1481997@gmail.com>
2022-12-21 10:55:17 +0200

    add column group name to create and edit blade

--- resources/views/pages/student/partials/newLessonModal.blade.php
+++ resources/views/pages/student/partials/newLessonModal.blade.php
@@ -1,6 +1,5 @@
-
 <div class="modal fade" id="newLessonModal" tabindex="-1" role="dialog" aria-labelledby="newLessonModal"
-aria-hidden="true">
+     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -8,23 +7,25 @@ aria-hidden="true">
             </div>
             <div class="modal-body">
                 <form data-url="{{route('admin.syllabus.createNewLesson')}}" method="post" id="newLessonForm">
-                    
-                    {{-- <input type="hidden" name="student_lesson_id" id="student_lesson_id"> --}}
 
                     <div class="row mb-3">
                         <div class="col">
-                            <input class="text-dark form-control" id="from_chapter" name="from_chapter" placeholder="From Chapter" type="number">
+                            <input class="text-dark form-control" id="from_chapter" name="from_chapter"
+                                   placeholder="From Chapter" type="number">
                         </div>
                         <div class="col">
-                            <input class="text-dark form-control" id="to_chapter" name="to_chapter" placeholder="To Chapter" type="number">
+                            <input class="text-dark form-control" id="to_chapter" name="to_chapter"
+                                   placeholder="To Chapter" type="number">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <input class="text-dark form-control" id="from_page" name="from_page" placeholder="From Page" type="number">
+                            <input class="text-dark form-control" id="from_page" name="from_page"
+                                   placeholder="From Page" type="number">
                         </div>
                         <div class="col">
-                            <input class="text-dark form-control" id="to_page" name="to_page" placeholder="To Page" type="number">
+                            <input class="text-dark form-control" id="to_page" name="to_page" placeholder="To Page"
+                                   type="number">
                         </div>
                     </div>
 
