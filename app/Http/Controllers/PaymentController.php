b61db9ac1596953b1ab529b4d870c6f485b4e618
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-12-27 00:17:23 +0200

    testing payments chart init

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,36 +10,26 @@ use Illuminate\Support\Facades\DB;
 use App\DataTables\PaymentDataTable;
 use RealRashid\SweetAlert\Facades\Alert;
 use App\Http\Requests\Payment\StorePaymentRequest;
-use App\Http\Requests\Payment\UpdatePaymentRequest;
 
 class PaymentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(PaymentDataTable $paymentDataTable)
     {
         return $paymentDataTable->render('pages.payment.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        $currentMonth = date('F');
+        $currentMonth = getCurrectMonthName();
 
         $gorups = Group::with([
-            'students',
+            'students.payments',
             'groupType',
             'payments' => function ($query) use ($currentMonth) {
                 return $query->where('month', $currentMonth);
             }
         ])->get();
+
         $gorups->map(function ($group) {
             $group->allStudentsPaid = $group->students->count() == $group->payments->where('paid', true)->count();
         });
@@ -50,12 +40,6 @@ class PaymentController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePaymentRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StorePaymentRequest $request)
     {
         if ($request->paid == "true") {
@@ -81,7 +65,6 @@ class PaymentController extends Controller
         return redirect()->back();
     }
 
-
     public function getMonthOfPayment(Request $request)
     {
         $payments = Payment::select(['id', 'paid', 'student_id', 'group_id', 'month'])
@@ -93,7 +76,6 @@ class PaymentController extends Controller
             'payments' => $payments
         ]);
     }
-
 
     public function getMonthCount(Request $request)
     {
@@ -107,51 +89,6 @@ class PaymentController extends Controller
         ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Payment $payment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Payment $payment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePaymentRequest  $request
-     * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePaymentRequest $request, Payment $payment)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Payment $payment)
-    {
-        //
-    }
-
     public function getPaymentPerMonthThisYear(Request $request)
     {
 
@@ -161,7 +98,7 @@ class PaymentController extends Controller
             DB::raw("(SUM(amount)) as month_amount"),
             'month'
         )->where('paid', 1);
-
+ 
         if (isset($request->start_time) && isset($request->end_time)) {
             $paymentsChart = $paymentsChart->whereBetween('created_at', [
                 Carbon::createFromFormat('Y-m-d', $request->start_time)->startOfDay()->toDateTimeString(),
--- tests/Traits/TestPaymentTrait.php
+++ tests/Traits/TestPaymentTrait.php
@@ -12,4 +12,49 @@ Trait TestPaymentTrait
             'group_id' => $group_id
         ]);
     }
+
+    private function generateRandomPaymentsData($created_at = null)
+    {
+        $group = $this->generateRandomGroup();
+        $student = $this->generateRandomStudent();
+        return [
+            'group_id' => $group->id,
+            'student_id' => $student->id,
+            'amount' => $group->groupType->price,
+            'month' => fake()->randomElement(getMonthNames()),
+            'paid' => fake()->boolean(),
+            'created_at' => $created_at || now()
+        ];
+    }
+
+    private function generateRandomPaymentsDataCustomed(array $configs)
+    {
+        $group = $this->generateRandomGroup();
+        $student = $this->generateRandomStudent();
+
+        $data = [
+            'group_id' => $group->id,
+            'student_id' => $student->id,
+            'amount' => $group->groupType->price,
+            'month' => fake()->randomElement(getMonthNames()),
+            'paid' => fake()->boolean(),
+        ];
+
+        foreach($configs as $columnName => $value)
+        {
+            $data[$columnName] = $value;
+        }
+        return $data;
+    }
+
+    private function generateRandomPaymentsCustomed(array $configs = [], int $count = 1)
+    {
+        $data = $this->generateRandomPaymentsDataCustomed($configs);
+
+        if($count == 1)
+        {
+            return Payment::factory()->create($data);
+        }
+        return Payment::factory($count)->create($data);
+    }
 }
