25b07e4f62304ec96a519052c4c9c71cbdecc4f6
AhmedKamal148 <a.kamal1481997@gmail.com>
2022-12-31 20:24:44 +0200

    Check If Teacher Exist

--- app/Http/Requests/Experience/StoreExperienceRequest.php
+++ app/Http/Requests/Experience/StoreExperienceRequest.php
@@ -6,28 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreExperienceRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
             'title' => 'required',
             'from' => 'required|date|date_format:Y-m-d|before:today|before:to',
             'to' => 'required|date|date_format:Y-m-d|before:today|after:from',
-            'teacher_id' => 'required',
+            'teacher_id' => 'required|exists:teachers,id',
         ];
     }
 }
--- app/Http/Requests/Teacher/StoreTeacherRequest.php
+++ app/Http/Requests/Teacher/StoreTeacherRequest.php
@@ -6,28 +6,20 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreTeacherRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+
+    public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
-    public function rules()
+
+    public function rules(): array
     {
         return [
-            'name'          => 'required',
-            'avatar'        => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
-            'birthday'      => 'required',
-            'phone'         => 'required',
+            'name' => 'required',
+            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
+            'birthday' => 'required',
+            'phone' => 'required',
             'qualification' => 'required',
         ];
     }
