b5bfbdc2f7172d054f133f7968048af937f0f18f
M0hamedSharaf <sazxsazx987987@gmail.com>
2023-01-01 13:06:06 +0200

    86696n8ky_add payment to factorySeeder(changes dbseeder)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -48,10 +48,9 @@ class DatabaseSeeder extends Seeder
 
 
         $this->call([
-            // QuraanSeeder::class,
-            // TgweedSeeder::class,
-            // QaidaNooraniahSeeder::class,
-            FactorySeeder::class,
+            QuraanSeeder::class,
+            TgweedSeeder::class,
+            QaidaNooraniahSeeder::class,
         ]);
     }
 }
