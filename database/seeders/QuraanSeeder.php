74450be62819dbf66c8067f0992fee7bdb72f4f9
M0hamedSharaf <sazxsazx987987@gmail.com>
2023-01-11 21:44:53 +0200

    new edit task quraanSeeder

--- database/seeders/QuraanSeeder.php
+++ database/seeders/QuraanSeeder.php
@@ -16,40 +16,16 @@ class QuraanSeeder extends Seeder
     public function run()
     {
 
-
-        // there is a problem I think her name "in pluse one but unfortunately I can't solve it  "
         $subject = Subject::upsert([
             'name' => 'القرآن الكريم'
         ], []);
-        // dd($subject);
 
-
-        //---------------------------- this is olde code--------------------------// 
-        // foreach ($chapters as $chapter) {
-        //     Lesson::upsert([
-        //         'title' => $chapter['surah_ar'],
-        //         'subject_id' => $subject->id,
-        //     ], [
-        //         'chapters_count' => $chapter['surah_count'],
-        //         'from_page' => $chapter['from_page'],
-        //         'to_page' => $chapter['to_page'],
-        //     ]);
-        // }
-        //--------------------------------------------------------------------//             
-
-
-
-        //this my new code 
-        // problem solving is 
-        // 1- recored in DataBase Id for Lesson 
-        // 2- prevent recurrnece in DataBase for table Lesson
         $chapters = chapterQuran();
-        $chapterarray = [];
+        $chaptersarray = [];
 
-        foreach ($chapters as $lesson_id => $chapter) {
+        foreach ($chapters as  $chapter) {
 
-            $chapterarray[] = [
-                'id' => $lesson_id + 1,
+            $chaptersarray[] = [
                 'title' => $chapter['surah_ar'],
                 'subject_id' => $subject->id,
                 'chapters_count' => $chapter['surah_count'],
@@ -57,16 +33,9 @@ class QuraanSeeder extends Seeder
                 'to_page' => $chapter['to_page'],
             ];
         }
-
         Lesson::upsert(
-            $chapterarray,
-            ['lesson_id'],
-            ['title'],
-            ['subject_id'],
-            ['chapters_count'],
-            ['from_page'],
-            ['to_page'],
+            $chaptersarray,
+            ['title', 'subject_id']
         );
-        // dd($chapterarray);
     }
 }
