323888b1212fdf52fc71e236e2c55c780fb916ca
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-01-26 00:18:14 +0200

    updates

--- resources/views/pages/role/partials/permissions.blade.php
+++ resources/views/pages/role/partials/permissions.blade.php
@@ -15,14 +15,11 @@
             @foreach (getPermissionsForView() as $table => $permissions)
                 <tr>
                     <td>
-                        {{-- <label for="{{$table}}">{{$table}}</label> --}}
                         <label class="control control-checkbox">
                             {{$table}}
                             <input type="checkbox" name="permissions[{{$table}}][]" id="{{$table}}" class="checkAll auto" data-table="{{$table}}"/>
                             <div class="control_indicator"></div>
                         </label>
-
-                        {{-- <input type="checkbox" id="{{$table}}" class="custom-checkbox checkAll" data-table="{{$table}}"> --}}
                     </td>
                     @foreach ($permissions as $permission)
                         <td>
@@ -31,7 +28,6 @@
                                     <input type="checkbox" class="permissionCheckbox" name="permissions[{{$table}}][]" value="{{$permission['value']}}" data-table="{{$table}}"/>
                                 <div class="control_indicator"></div>
                             </label>
-                            {{-- <input type="checkbox" name="{{$table}}[]" value="{{$permission['value']}}"> --}}
                         </td>
                     @endforeach
                 </tr>
@@ -59,16 +55,16 @@
         $(".permissionCheckbox").change(function(){
                 let table = $(this).data('table')
                 
-                let allCheckbox = document.querySelectorAll(`input[name='permissions[${table}][]']:not([id="${table}"])`);
+                let allCheckboxes = document.querySelectorAll(`input[name='permissions[${table}][]']:not([id="${table}"])`);
                 let checkboxChecked  = document.querySelectorAll(`input[name='permissions[${table}][]']:not([id="${table}"]):checked`);
 
-                if (allCheckbox.length ==  checkboxChecked.length ) 
+                if (allCheckboxes.length ==  checkboxChecked.length ) 
                 {
                     $(`input[name='permissions[${table}][]']`).prop('checked',true)
                 }
                 else
                 {
-                    $('.checkAll').prop('checked',false)
+                    $(`#${table}`).prop('checked',false)
                 }
           });
     </script>
