3dba919663d6ade5a93b6a501731395046f347e1
AhmedKamal148 <a.kamal1481997@gmail.com>
2023-02-02 08:51:07 +0200

    fix some issues at student

---
+++ app/Rules/Global/CheckIsFaieldString.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Rules\Global;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CheckIsFaieldString implements Rule
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function passes($attribute, $value) :bool
+    {
+        return preg_match('/[^0-9]/',$value);
+    }
+
+
+    public function message() :string
+    {
+        return 'This Failed Doesn\'t Accept Numbers Or Special Characters ';
+    }
+}
