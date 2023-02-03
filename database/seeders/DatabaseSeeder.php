2fb02f0d443ffd121ad39e95b948bb7a31160c79
mohamed sharaf <sazxsazx987987@gmail.com>
2023-02-03 16:26:41 +0200

    move group types to their own seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,26 +18,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(AdminSeeder::class);
         
-        GroupType::updateOrCreate([
-            'name' => 'normal',
-        ], [
-            'price' => 80,
-            'days_num' => 2
-        ]);
-
-        GroupType::updateOrCreate([
-            'name' => 'dense',
-        ], [
-            'price' => 120,
-            'days_num' => 4
-        ]);
-
-        GroupType::updateOrCreate([
-            'name' => 'all week',
-        ], [
-            'price' => 200,
-            'days_num' => 6
-        ]);
+        $this->call(GroupTypeSeeder::class);
 
 
         $this->call([
