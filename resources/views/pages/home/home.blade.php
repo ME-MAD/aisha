2cc66aeef75ada621ac813f536158c6d59d64566
mohamed sharaf <sazxsazx987987@gmail.com>
2023-02-11 04:19:52 +0200

    home colors and breadcrump

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -11,36 +11,28 @@
     <link href="{{asset('adminAssets/plugins/flatpickr/flatpickr.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('adminAssets/plugins/flatpickr/custom-flatpickr.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('adminAssets/assets/css/forms/theme-checkbox-radio.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('adminRtl/assets/css/elements/breadcrumb.css')}}" rel="stylesheet" type="text/css">
+
     <!-- END THEME GLOBAL STYLES -->
 @endpush
 
 @section('breadcrumb')
-    <div class="page-header">
-        <div class="page-title">
-            <h3>{{ __('home.Analytics Dashboard') }}</h3>
-        </div>
-        <div class="dropdown filter custom-dropdown-icon">
-            <a class="dropdown-toggle btn" href="#" role="button" id="filterDropdown" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false"><span class="text"><span>Show</span> : Daily Analytics</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="feather feather-chevron-down">
-                    <polyline points="6 9 12 15 18 9"></polyline>
-                </svg>
-            </a>
+<div class="col-md-12">
+    <div class="breadcrumb bg-transparent card-header home">
 
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="filterDropdown">
-                <a class="dropdown-item" data-value="<span>Show</span> : Daily Analytics" href="javascript:void(0);">Daily
-                    Analytics</a>
-                <a class="dropdown-item" data-value="<span>Show</span> : Weekly Analytics" href="javascript:void(0);">Weekly
-                    Analytics</a>
-                <a class="dropdown-item" data-value="<span>Show</span> : Monthly Analytics"
-                    href="javascript:void(0);">Monthly Analytics</a>
-                <a class="dropdown-item" data-value="Download All" href="javascript:void(0);">Download All</a>
-                <a class="dropdown-item" data-value="Share Statistics" href="javascript:void(0);">Share Statistics</a>
-            </div>
+        <div class="breadcrumb-four">
+            <ul class="breadcrumb">
+                <li class="active">
+                    <a href="{{route('admin.home')}}"
+                       class="d-flex justify-content-center align-items-center">
+                        <i class="fa-solid fa-house mx-2 fa-2x"></i>
+                        <span class="font-weight-bold mt-1">الصفحة الرئيسية</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
+</div>
 @endsection
 
 @section('content')
