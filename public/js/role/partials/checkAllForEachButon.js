2c2531ad017ad27010a57feb33a6d8d7b2e07bf2
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-02-28 06:13:42 +0200

    fix permissions checkbox bug

--- public/js/role/partials/checkAllForEachButon.js
+++ public/js/role/partials/checkAllForEachButon.js
@@ -15,13 +15,15 @@ export function permissionCheckbox()
     $(".permissionCheckbox").change(function () {
         let table = $(this).data('table')
 
+        console.log(table);
+
         let allCheckboxes = document.querySelectorAll(`input[name='permissions[${table}][]']:not([id="${table}"])`);
         let checkboxChecked = document.querySelectorAll(`input[name='permissions[${table}][]']:not([id="${table}"]):checked`);
 
         if (allCheckboxes.length == checkboxChecked.length) {
             $(`input[name='permissions[${table}][]']`).prop('checked', true)
         } else {
-            $(`#${table}`).prop('checked', false)
+            $(`#${table}.checkAll`).prop('checked', false)
         }
     });
 }
